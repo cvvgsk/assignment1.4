93858ae08ad75cfe01dc140a59b7b622d4c2289e
sai koushik <sai koushik>
2016-12-18 22:21:19 +0530

    assignment1.4

---
+++ src/ascii.java
@@ -0,0 +1,12 @@
+
+public class ascii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=65;i<=90;i++){
+System.out.println((char)i);
+		}
+	}
+
+}
